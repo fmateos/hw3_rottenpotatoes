af381b2ae4984556ba1b08de71f1c47714f74321
fmateos <francisco.mateos@gmail.com>
2012-08-08 15:28:37 +0200

    Update features/step_definitions/movie_steps.rb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -27,3 +27,7 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
 end
+Then /I should see all of the movies/
+  
+
+end
